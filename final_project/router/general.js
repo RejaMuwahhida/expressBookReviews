6f968cc3274d75c0fa19ed7e442bb24dcfec2ba5
Reja Muwahhida <98947123+RejaMuwahhida@users.noreply.github.com>
2023-08-13 07:12:32 -0700

    Update general.js by  using promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,35 +1,40 @@
 const express = require('express');
+const fs = require('fs');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
 
   const username = req.body.username;
   const password = req.body.password;
+
  if(username && password){
   if(isValid(username)){
-
     return res.status(404).json({message : "user alredy exists"});
-  }
-  else{
+  }else{
     users.push({"username":username,"password":password});
     return res.status(200).json({message : "user successfully registered,now you can login"});
   }
- }
- else{
+ }else{
   return res.status(404).json({message: "Unable to register"});
  }
-
-
-
 });
 
 // Get the book list available in the shop
+function getbooks(){
+    return new Promise((resolve,reject)=>{
+        const bookstosend = books;
+        resolve(bookstosend);
+    });
+}
 public_users.get('/',function (req, res) {
-   res.send(JSON.stringify(books,null,4));
+    getbooks().then(bookstosend=>{
+        res.send(JSON.stringify(bookstosend,null,4));
+    }).catch(error =>{
+        res.status(500).send("error");
+    });
 });
 
 public_users.get('/user',function (req, res) {
@@ -38,39 +43,69 @@ public_users.get('/user',function (req, res) {
  
 
 // Get book details based on ISBN
+function getbook(isbn){
+    return new Promise((resolve,reject)=>{
+        let book = books[isbn];
+        if(book){
+         resolve(book);
+        }else{
+          reject("error");
+        }
+    });
+}
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  let book = books[isbn];
-  if(book){
-    res.json(book);
-  }else{
-    res.send("wrong isbn given");
-  }
-
- });
+    const isbn = req.params.isbn;
+    getbook(isbn).then(book=>{
+        res.json(book);
+    }).catch(error=>{
+        res.status(500).send(`${error}`);
+        res.send("wrong isbn given");
+    });
+});
   
 // Get book details based on author
+function getBookByAuthor(author){
+    return new Promise((resolve,reject)=>{
+        let searched_book = Object.values(books).filter((book)=>book.author===author);
+        if(searched_book.length>0){
+          resolve(searched_book[0]);
+        }else{
+          reject("no such author found");
+        }
+    });
+}
+
 public_users.get('/author/:author',function (req, res) {
 
   const author = req.params.author;
-  let searched_book = Object.values(books).filter((book)=>book.author===author);
-  if(searched_book.length>0){
-    res.send(searched_book[0]);
-  }else{
-    res.send("no such author found");
-  }
+  getBookByAuthor(author).then(book=>{
+    res.json(book);
+}).catch(error=>{
+    res.status(500).send(`${error}`);
+});
 });
 
 // Get all books based on title
+function getBookByTitle(title){
+    return new Promise((resolve,reject)=>{
+        let searched_book = Object.values(books).filter((book)=>book.title===title);
+        if(searched_book.length>0){
+          resolve(searched_book[0]);
+        }else{
+          reject("no such title found");
+        }
+       
+    });
+}
+
 public_users.get('/title/:title',function (req, res) {
 
   const title= req.params.title;
-  let searched_book = Object.values(books).filter((book)=>book.title===title);
-  if(searched_book.length>0){
-    res.send(searched_book[0]);
-  }else{
-    res.send("no such book available");
-  }
+  getBookByTitle(title).then(book=>{
+    res.json(book);
+}).catch(error=>{
+    res.status(500).send(`${error}`);
+});
 });
 
 //  Get book review
